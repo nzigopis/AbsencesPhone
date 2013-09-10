c2280f449e43dbe1f8e98f6aef8db9e8fe656c6e
Nikolaos Zigopis <nzigopis@gmail.com>
2013-09-10 10:45:20 +0300

    //

---
+++ js/PageStateManager.js
@@ -0,0 +1,49 @@
+﻿PageStateManager = (function () {
+    var viewModels = {};
+
+    var initPage = function (page, viewModel) {
+
+        // If binding exists, return
+        if (ko.dataFor(page[0]))
+            return;
+
+        if (viewModel) {
+            // If binding viewModel provided, apply binding
+            ko.applyBindings(viewModel, page[0]);
+        }
+        else {
+            // No binding provided, try to create a default from "data-viewModel" attribute
+            var viewModelName = page.attr("data-viewModel");
+
+            if (viewModelName) {
+                viewModel = eval('new ' + viewModelName + '()');
+                ko.applyBindings(viewModel, page[0]);
+            }
+            else
+                Console.log('Error moving to page ' + page[0] + '. No view model provided !');
+        };
+
+    };
+
+    var changePage = function (url, viewModel) {
+        //Console.log('Enter changePage ' + url);
+        $.support.cors = true;
+        $.mobile.allowCrossDomainPages = true;
+
+        $.mobile.changePage(url, { viewModel: viewModel });
+        //Console.log('Exit changePage ' + url);
+    };
+
+    var onPageChange = function (e, info) {
+        //Console.log('Enter onPageChange=' + info.toPage);
+        initPage(info.toPage, info.options.viewModel);
+        //Console.log('Exit onPageChange');
+    };
+
+    $(document).bind("pagechange", onPageChange);
+
+    return {
+        initPage: initPage,
+        changePage: changePage
+    };
+})();
---
+++ js/ViewModels/IndexViewModel.js
@@ -0,0 +1,59 @@
+﻿IndexViewModel = function () {
+
+    // Console.log('new LoginViewModel()');
+
+    this.userName = ko.observable("");
+    this.userPassword = ko.observable("");
+    this.loginMessage = ko.observable("");
+
+    this.login = function () {
+        var errorLabel = this.loginMessage;
+        errorLabel("");
+
+        var user = this.userName();
+        var pwd = this.userPassword();
+
+        $.mobile.loading('show');
+        // PageStateManager.changePage('main.html', new MainViewModel(partner));
+		var db = openDatabase('apousies.db', '1.0', 'Test DB', 2 * 1024 * 1024);
+		db.transaction(function (tx) {
+			try
+			{
+				db.transaction(function (tx) {
+				   tx.executeSql('CREATE TABLE IF NOT EXISTS USERS (userName unique, userPassword)');
+				});
+				// 
+				db.transaction(function (tx) {
+					tx.executeSql('SELECT * FROM USERS', [], function (tx, results) {
+						var len = results.rows.length, i;
+						if (len == 0)
+						{
+							tx.executeSql('INSERT INTO USERS (userName, userPassword) VALUES (?, ?)', ['nikos', 'zigopis']);
+							LoggedOnUser.init('nikos', 'zigopis');
+							PageStateManager.changePage('main.html');
+							$.mobile.loading('hide');
+							return;
+						}
+						for (i = 0; i < len; i++){
+							if (user === results.rows.item(i).userName && pwd === results.rows.item(i).userPassword) {
+								LoggedOnUser.init(user, pwd);
+								PageStateManager.changePage('main.html');
+								$.mobile.loading('hide');
+							}
+							else
+							{
+								errorLabel("Λάθος Στοιχεία !");
+								$.mobile.loading('hide');
+							}
+						}
+					}, null);
+				});
+			}
+			catch(err)
+			{
+				this.loginMessage(JSON.stringify(err));
+			}
+		});
+    };
+
+}
