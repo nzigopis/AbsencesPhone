b229118a561cd7a1d2f6a8152f282a96dbc5d075
Nikolaos Zigopis <nzigopis@gmail.com>
2013-09-11 13:10:05 +0300

    Added DbSync

---
+++ js/Services/DbSync.js
@@ -0,0 +1,50 @@
+DbSync = (function () {
+    
+	var initialize = function (userName, userPassword) {
+		
+		var db = openDatabase('absences.db', '1.0', 'Test DB', 2 * 1024 * 1024);
+		
+		db.transaction(function (tx) {
+			
+			db.transaction(function (tx) {
+			   tx.executeSql('CREATE TABLE IF NOT EXISTS USERS (userName unique, userPassword)');
+			});
+			// 
+			db.transaction(function (tx) {
+				tx.executeSql('SELECT * FROM USERS', [], function (tx, results) {
+					var len = results.rows.length, i;
+					if (len === 0)
+					{
+						tx.executeSql('INSERT INTO USERS (userName, userPassword) VALUES (?, ?)', ['nikos', 'zigopis']);
+						return true;
+					}
+					for (i = 0; i < len; i++){
+						if (userName === results.rows.item(i).userName && 
+							userPassword === results.rows.item(i).userPassword) {
+							return true;
+						}
+						else
+						{
+							throw("Λάθος Στοιχεία !");
+						}
+					}
+				}, null);
+			});
+			
+		});
+    };
+	
+    var pull = function (userName, userPassword, url) {
+		return initialize(userName, userPassword);
+    };
+
+    var push = function (userName, userPassword, url) {
+
+	};
+
+    return {
+        pull: pull,
+        push: push
+    };
+})();
+
--- js/ViewModels/IndexViewModel.js
+++ js/ViewModels/IndexViewModel.js
@@ -13,47 +13,22 @@ IndexViewModel = function () {
         var user = this.userName();
         var pwd = this.userPassword();
 
-        $.mobile.loading('show');
-        
-		var db = openDatabase('apousies.db', '1.0', 'Test DB', 2 * 1024 * 1024);
-		db.transaction(function (tx) {
-			try
-			{
-				db.transaction(function (tx) {
-				   tx.executeSql('CREATE TABLE IF NOT EXISTS USERS (userName unique, userPassword)');
-				});
-				// 
-				db.transaction(function (tx) {
-					tx.executeSql('SELECT * FROM USERS', [], function (tx, results) {
-						var len = results.rows.length, i;
-						if (len === 0)
-						{
-							tx.executeSql('INSERT INTO USERS (userName, userPassword) VALUES (?, ?)', ['nikos', 'zigopis']);
-							LoggedOnUser.init('nikos', 'zigopis');
-							PageStateManager.changePage('class.html');
-							$.mobile.loading('hide');
-							return;
-						}
-						for (i = 0; i < len; i++){
-							if (user === results.rows.item(i).userName && pwd === results.rows.item(i).userPassword) {
-								LoggedOnUser.init(user, pwd);
-								PageStateManager.changePage('class.html');
-								$.mobile.loading('hide');
-							}
-							else
-							{
-								errorLabel("Λάθος Στοιχεία !");
-								$.mobile.loading('hide');
-							}
-						}
-					}, null);
-				});
-			}
-			catch(err)
-			{
-				this.loginMessage(JSON.stringify(err));
-			}
-		});
+        try
+		{
+			$.mobile.loading('show');
+        	
+			DbSync.pull(user, pwd);
+			LoggedOnUser.init('nikos', 'zigopis');
+			PageStateManager.changePage('class.html', {});
+		}
+		catch(err)
+		{
+			this.loginMessage(JSON.stringify(err));
+		}
+		finally
+		{
+			$.mobile.loading('hide');
+		}
     };
 
 }
