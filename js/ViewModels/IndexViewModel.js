5fbceaaf9ed6c38b5e9a3e8ad08f8f9b2a677e95
nzigopis <nzigopis@192.168.0.33>
2013-09-12 09:00:17 +0300

    //

--- js/ViewModels/IndexViewModel.js
+++ js/ViewModels/IndexViewModel.js
@@ -1,7 +1,5 @@
 IndexViewModel = function () {
 
-    // Console.log('new LoginViewModel()');
-
     this.userName = ko.observable("");
     this.userPassword = ko.observable("");
     this.loginMessage = ko.observable("");
@@ -14,21 +12,46 @@ IndexViewModel = function () {
         var pwd = this.userPassword();
 
         try
-		{
-			$.mobile.loading('show');
-        	
-			DbSync.pull(user, pwd);
-			LoggedOnUser.init('nikos', 'zigopis');
-			PageStateManager.changePage('class.html', {});
-		}
-		catch(err)
-		{
-			this.loginMessage(JSON.stringify(err));
-		}
-		finally
-		{
-			$.mobile.loading('hide');
-		}
+        {
+            $.mobile.loading('show');
+
+            openDatabase('absences.db', '1.0', 'Test DB', 2 * 1024 * 1024, function (db) {
+                db.transaction(function (tx) {
+                    var select = 'SELECT * FROM USERS';
+                    tx.executeSql(select, [], function (tx, results) {
+                        var len = results.rows.length, i;
+                        if (len === 0)
+                        {
+                            DbSync.pull(tx, user, pwd, errorCallback);
+                            return true;
+                        }
+                        for (i = 0; i < len; i++){
+                            if (user === results.rows.item(i).userName && 
+                                pwd === results.rows.item(i).userPassword) {
+                                return true;
+                            }
+                            else
+                            {
+                                throw("Λάθος Στοιχεία !");
+                            }
+                        }
+                    }, function (tx, e) {
+                        // Load from server
+                        DbSync.pull(tx, user, pwd, errorCallback);
+                    });
+                });
+            });
+            LoggedOnUser.init('nikos', 'zigopis');
+            PageStateManager.changePage('class.html', {});
+        }
+        catch(err)
+        {
+            this.loginMessage(JSON.stringify(err));
+        }
+        finally
+        {
+            $.mobile.loading('hide');
+        }
     };
 
 }
