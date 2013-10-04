180f8988fc2a48f97ff8d9e3c864dae8de4d5249
Nikolaos Zigopis <nzigopis@gmail.com>
2013-10-04 13:16:02 +0300

    StudentInfo without Id

--- js/Models/Student.js
+++ js/Models/Student.js
@@ -4,14 +4,13 @@ Student = function (studentId, firstName, lastName, fatherName, motherName,
     this.studentId = studentId;
     this.firstName = firstName;
 	this.lastName = lastName;
-	this.fatherName = fatherName;
-	this.motherName = motherName;
+	this.fatherName = fatherName || '';
+	this.motherName = motherName || '';
 	this.unexcusedAbsencesCount = unexcusedAbsencesCount || 0;
 	this.excusedAbsencesCount = excusedAbsencesCount || 0;
 	
 	this.studentInfo = function(){
-		return this.studentId + ' ' + this.lastName + ' ' + this.firstName ; 
-//				this.unexcusedAbsencesCount + '(ΑΔΙΚ.)' + this.excusedAbsencesCount + '(ΔΙΚ.)';
+		return this.lastName + ' ' + this.firstName + ' ' + this.fatherName.substring(0, 3); 
 	};
 	
 	this.studentAbsences = function(){
